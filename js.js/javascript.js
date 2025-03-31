3ab4cfbea29476c36ec32eba2e625589a81c3ace
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-31 07:12:10 +0200

    [UPD] PESO MINORE

--- js.js/javascript.js
+++ js.js/javascript.js
@@ -1,14 +1,26 @@
-const biciclette = [ 
+const biciclette = [
     {
-        nome : 'Focus' ,
-        peso : 8.10
+        nome: 'Focus',
+        peso: 8.10
     },
     {
-        nome : 'Yeti' ,
-        peso : 13.88
+        nome: 'Yeti',
+        peso: 13.88
     },
     {
-        nome : 'Look' ,
-        peso : 7.88 
+        nome: 'Look',
+        peso: 7.88
     },
 ]
+
+let pesoMinore = biciclette[0].peso;
+let minore;
+
+for (let i = 0; i < biciclette.length; i++) {
+    if (pesoMinore > biciclette[i].peso) {
+        minore = biciclette[i];
+        pesoMinore = biciclette[i].peso;
+    }
+}
+
+console.log(minore);
