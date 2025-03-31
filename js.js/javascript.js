cb791d28491be97f441d669539bed0ccd063ffa7
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-31 06:20:38 +0200

    [UPD] ARRAY CREATO

---
+++ js.js/javascript.js
@@ -0,0 +1,14 @@
+const biciclette = [ 
+    {
+        nome : 'Focus' ,
+        peso : 8.10
+    },
+    {
+        nome : 'Yeti' ,
+        peso : 13.88
+    },
+    {
+        nome : 'Look' ,
+        peso : 7.88 
+    },
+]
